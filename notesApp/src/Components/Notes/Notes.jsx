e2d7d742c6cef03657775520fd8bb68c54373027
Han <icyfarhan@gmail.com>
2024-09-17 12:21:13 +0700

    Completed the design for the Active Notes and Archived Notes

--- notesApp/src/Components/Notes/Notes.jsx
+++ notesApp/src/Components/Notes/Notes.jsx
@@ -4,6 +4,82 @@ export default function Notes() {
   return (
     <>
       <div className={styles.parentContainer}>
+        <h1>Active Notes</h1>
+        <div className={styles.childContainer}>
+          <div className={styles.contentContainer}>
+            <h1>Test 1</h1>
+            <div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Deserunt fuga ipsa maiores perferendis possimus quisquam
+                  tempore! Accusantium atque consequuntur dolor doloribus eum
+                  ipsam molestias neque odit optio, quod reprehenderit
+                  temporibus.
+                </p>
+              </div>
+              <div className={styles.buttonGroup}>
+                <button>Delete Note</button>
+                <button>Archive Note</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.contentContainer}>
+            <h1>Test 1</h1>
+            <div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Deserunt fuga ipsa maiores perferendis possimus quisquam
+                  tempore! Accusantium atque consequuntur dolor doloribus eum
+                  ipsam molestias neque odit optio, quod reprehenderit
+                  temporibus.
+                </p>
+              </div>
+              <div>
+                <button>Delete Note</button>
+                <button>Archive Note</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.contentContainer}>
+            <h1>Test 1</h1>
+            <div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Deserunt fuga ipsa maiores perferendis possimus quisquam
+                  tempore! Accusantium atque consequuntur dolor doloribus eum
+                  ipsam molestias neque odit optio, quod reprehenderit
+                  temporibus.
+                </p>
+              </div>
+              <div className={styles.buttonGroup}>
+                <button>Delete Note</button>
+                <button>Archive Note</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.contentContainer}>
+            <h1>Test 1</h1>
+            <div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Deserunt fuga ipsa maiores perferendis possimus quisquam
+                  tempore! Accusantium atque consequuntur dolor doloribus eum
+                  ipsam molestias neque odit optio, quod reprehenderit
+                  temporibus.
+                </p>
+              </div>
+              <div className={styles.buttonGroup}>
+                <button>Delete Note</button>
+                <button>Archive Note</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h1>Archived Notes</h1>
         <div className={styles.childContainer}>
           <div>
             <h1>Test 1</h1>
