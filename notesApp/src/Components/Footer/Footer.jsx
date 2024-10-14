19a947b817c66d026ca09f374c2e2ae85ebe3930
han <icyfarhan@gmail.com>
2024-10-14 13:53:58 +0700

    Imported style small fix, it's not used before

--- notesApp/src/Components/Footer/Footer.jsx
+++ notesApp/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../../assets/github-mark-white.svg";
 export default function Footer() {
   return (
     <>
-      <footer>
+      <footer className={styles.footer}>
         <img src={logo} alt="" />
         <a href="https://github.com/hangodek/project_Notes/commits/main/">
           hangodek - Click here to see the commit log
